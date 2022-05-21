72bcc665c9901e04c30fa7693860c8c2319ec257
Ibrahim Shady <96144600+ABSTRAC7ION@users.noreply.github.com>
2022-05-21 23:59:01 +0200

    fix

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer().single("upfile");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload, async function (req, res) {
+  if (req.file) {
+    // destructure the pertinent information and return a JSON object
+    const { originalname: name, mimetype: type, size } = req.file;
+    res.json({
+      name,
+      type,
+      size,
+    });
+  }
 });
 
 
